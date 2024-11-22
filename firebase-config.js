18d10629c34552c2e5f0e5b75d03955fabf9733f
Ferney <ferney.montoyag@gmail.com>
2024-11-22 16:26:54 -0500

    Authentication google android service config setted but not working :(

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import second from 'firebase'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDHgjmRKSccH-4ZiW1E5qlUSIpDA5AswAc",
@@ -9,13 +8,12 @@ const firebaseConfig = {
   storageBucket: "money-tracker-2b01f.firebasestorage.app",
   messagingSenderId: "309334451456",
   appId: "1:309334451456:web:faf3c02dea77869a113fe5",
-  measurementId: "G-WNHBLLNS6P",
+  measurementId: "G-WNHBLLNS6P"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta el Auth para usarlo en otras partes
-const auth = getAuth(app);
+// IOS: 756887444943-lkb560mpndkiata6h7taoqr70aq4cfgk.apps.googleusercontent.com
 
-export { app, auth };
+// Android: 756887444943-rr48qdprb7nuqmr8q0t3hvv5b29te4s0.apps.googleusercontent.com
+
